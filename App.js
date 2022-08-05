45d2004adb03cb18ed59136f066282d45e94c8e6
epiii <gue.elfrianto@gmail.com>
2022-08-06 05:52:52 +0700

    install react native material & implement gmap api

--- App.js
+++ App.js
@@ -1,32 +1,82 @@
+import {
+  Button,
+  Text,
+  Flex,
+  Box,
+  AppBar,
+  ListItem
+  // IconButton,
+  // Icon
+} from "@react-native-material/core";
 import React from 'react';
 import {
-  StyleSheet,
+  SafeAreaView,
+  StyleSheet, IconComponentProvider,
+  //  Text, 
   View
 } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
   return (
-    <View style={styles.wrapper}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: -7.2458404,
-          longitude: 112.7378177,
-          latitudeDelta: 0.03,
-          longitudeDelta: 0.03
-        }}
-      >
-        <Marker
-          coordinate={{
-            latitude: -7.245052805728057,
-            longitude: 112.73764603863215
+    <SafeAreaView>
+      <AppBar
+        color='blue'
+        title="Google Map"
+      // leading={() => (
+      //   <IconButton
+      //     icon={() => <Ionicons name="ios-person-sharp" size={30} color="#4F8EF7" />
+      //     } />
+      // )}
+      />
+      <ListItem title="List Item" />
+      <ListItem title="List Item" />
+
+      <Flex fill>
+        <Box h={12} style={{ backgroundColor: 'lightgreen' }}>
+          <Text>
+            halo
+          </Text>
+
+        </Box>
+        <Box h={12} style={{ backgroundColor: 'lightblue' }}>
+          <Text>Box 2</Text>
+        </Box>
+        <Flex fill style={{ backgroundColor: 'tomato' }}>
+          <Text>Box 3</Text>
+        </Flex>
+      </Flex>
+
+      <View style={styles.wrapper}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: -7.2458404,
+            longitude: 112.7378177,
+            latitudeDelta: 0.03,
+            longitudeDelta: 0.03
           }}
-        />
-      </MapView>
-    </View>
+        >
+          <Marker
+            coordinate={{
+              latitude: -7.245052805728057,
+              longitude: 112.73764603863215
+            }}
+          />
+        </MapView>
+      </View>
+      <Button title="Click Me" style={{ alignSelf: "center", marginTop: 40 }} />
+      <Text>
+        halooo
+        halooo
+        halooo
+        halooo
+      </Text>
+    </SafeAreaView>
   );
 };
+
 
 const styles = StyleSheet.create({
   wrapper: {
